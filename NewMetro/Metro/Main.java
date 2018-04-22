97b308d0b76b5a59b6adb295ff2348c7b2e84924
Utilisateur <user@debian.arcahe.ovh>
2018-04-22 14:30:15 +0200

    push fichier debutant and expert

--- NewMetro/Metro/Main.java
+++ NewMetro/Metro/Main.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.Scanner;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.NoSuchElementException;
@@ -53,26 +54,60 @@ public class Main
         }
         return disMin;
     }
+    public static String menu()
+    {
+    	String fichier=null;
+    	int choix=0;
+    	Scanner sc=new Scanner(System.in);
+    	System.out.println("1.Skieur débutant");
+    	System.out.println("2.Skieur expert");
+    	System.out.println("Faites votre choix:");
+    	choix = sc.nextInt();
+    	switch(choix)
+    	{
+    		case 1:
+    			fichier ="debutant.txt";
+    			return fichier;
+    		case 2:
+    			fichier ="expert.txt";
+    			return fichier;
+    		default:
+    			System.out.println("Mauvais choix");return fichier;
+    	}
+    }
     
+    public static String[] sourceDestination()
+    {
+    	String str[] = new String[2];
+    	Scanner sc = new Scanner(System.in);
+    	System.out.print("Où êtes vous ? :");
+    	str[0] = sc.nextLine();
+    	System.out.print("Où allez vous? :");
+    	str[1] = sc.nextLine();
+    	return str;
+    }
     
     
     public static void main(String[] args)
     {
         ListeSommets ls;
-        ls = new ListeSommets(70);
+        ls = new ListeSommets(71);
         MatriceChemins matrice;
         Dijkstra d;
         int disMin = Integer.MAX_VALUE;
+        String file = menu();
+        String str[] = new String[2];
+        str = sourceDestination();
+        
 
         // test
         try{
-            matrice = new MatriceChemins(70);
+            matrice = new MatriceChemins(71);
             matrice.initialiser();
-            matrice.remplireMatrice("metro.txt");
-            ls.remplireTableauSommet("metro.txt");
-           
-            ls.idenSource("Col du Prorel");
-            ls.idenDestination("Col de la Ricelle");
+            matrice.remplireMatrice(file);
+            ls.remplireTableauSommet(file);
+            ls.idenSource(str[0]);
+            ls.idenDestination(str[1]);
            
             d = new Dijkstra(matrice, ls, ls.source[0], ls.destination[0]);
             
